8db1e35510a2b4433e22efa479e94d70b44dda68
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-06-15 18:00:35 +0000

    cambios1

---
+++ src/app.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+
+  var pronoun = ["Chocobuda", "Enfocando", "Jefe", Sísifo];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+};
+
+// Instrucciones
+// Crea un script que genere todas las combinaciones posibles de nombres de dominio de una lista de pronombres, adjetivos y sustantivos, por ejemplo:
+
+//   var pronoun = ['the','our'];
+//   var adj = ['great', 'big' ];
+//   var noun = ['jogger','racoon'];
+// Debes generar algo como:
+
+// thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com
+// 💡Sugerencias
+// Deberás usar los bucles o loops anidados para mezclar los diferentes valores.
+
+// Tus herramientas: Para bucles o loops, concatenación de strings.
+
+// 😎 ¿Te sientes con confianza?
+// Agrega varios tipos de extensiones, por ejemplo: .com, .net, .us, .io, etc.
+
+// Agrega "domain hacks" donde la extensión forma parte del dominio, por ejemplo: En lugar de puedes.com el "hack" sería pued.es porque sigue diciendo "puedes" pero se aprovecha la extensión .es.
