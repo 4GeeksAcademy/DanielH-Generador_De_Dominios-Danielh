9b6152151cbcd1c43ec048ed71f5303e1ca0e638
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-06-15 16:19:06 +0000

    limpieza de archivos

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
