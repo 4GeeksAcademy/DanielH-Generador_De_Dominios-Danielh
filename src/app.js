bb78f0adf145623ce5ecc5c5181e03a531f24253
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-06-16 18:17:51 +0000

    index modificado con boostrap

--- src/app.js
+++ src/app.js
@@ -9,22 +9,23 @@ window.onload = function() {
   //write your code here
   const dominio = [];
   var extensiones = [".com", ".es", ".net", ".org"];
-  var pronoun = ["Chocobuda", "Enfocando", "Jefe", "Sisifo"];
+  var pronoum = ["Chocobuda", "Enfocando", "Jefe", "Sisifo"];
   var adj = ["Código", "1001", "Hacker", "Gratis"];
-  var noun = ["Fashion", "Nómada", "Criatura", "Arquitectos"];
+  var noum = ["Fashion", "Nómada", "Criatura", "Arquitectos"];
 
   function generadorDominio() {
-    for (let i = 0; i < pronoun.length; i++) {
+    for (let i = 0; i < pronoum.length; i++) {
       for (let a = 0; a < adj.length; a++) {
-        for (let r = 0; r < noun.length; r++) {
+        for (let r = 0; r < noum.length; r++) {
           for (let e = 0; e < extensiones.length; e++) {
-            let domioRandom = pronoun[i] + adj[a] + noun[r] + extensiones[e];
+            let domioRandom = pronoum[i] + adj[a] + noum[r] + extensiones[e];
             dominio.push(domioRandom);
           }
         }
       }
     }
   }
+
   // const dominios =
   generadorDominio();
   const dominioElement = document.querySelector("#dominio");
